04436fa1e7255922a24eac6b23781fe0eef4037d
Yuval <yuval@Yuvals-MacBook-Pro.local>
2024-07-10 23:59:22 +0300

    ci: Add github actions

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,13 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/main.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'iife', // Immediately Invoked Function Expression
+    format: 'iife',
     sourcemap: true,
   },
   plugins: [
@@ -18,6 +19,12 @@ export default {
       babelHelpers: 'bundled',
       presets: ['@babel/preset-env'],
     }),
-    // terser(),
+    terser(),
+    copy({
+      targets: [
+        { src: 'style.css', dest: 'dist' },
+        { src: 'manifest.json', dest: 'dist' },
+      ]
+    }),
   ],
 };
