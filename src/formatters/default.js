651845ff0aa9e3ff285d6b5f15b3e0959dcc2767
Yuval <yuval@Yuvals-MacBook-Pro.local>
2024-07-12 22:58:29 +0300

    default: Add URI component decode

--- src/formatters/default.js
+++ src/formatters/default.js
@@ -9,6 +9,7 @@ export const DEFAULT_FORMATTERS = [
     new RegExpFormatter("Base64", /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/, (text) => atob(text)),
     new RegExpFormatter("IPv4", /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/, (text) => [
         urlElement(`https://ipinfo.io/${text}`, 'ipinfo.io'),
-        urlElement(`https://who.is/whois-ip/ip-address/${text}`, 'who.is')
+        urlElement(`https://who.is/whois-ip/ip-address/${text}`, 'who.is'),
     ]),
+    new RegExpFormatter("Encoded URI", /%[0-9a-fA-F]{2}/, (text) => decodeURIComponent(text)),
 ];
